a47efefc1df46f3a1a6c9984c0ff843952f6da79
Enrico Papi <enrico.papi@gmail.com>
2021-05-15 12:06:18 +0200

    version bump

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 #endif
 
 	QGuiApplication app(argc, argv);
-	app.setApplicationVersion("1.0");
+    app.setApplicationVersion("1.1");
 
 	QQmlApplicationEngine engine;
 	const QUrl url(QStringLiteral("qrc:/main.qml"));
